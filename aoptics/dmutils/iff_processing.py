910b002a013b9c7fc5df1c8a909b552e02df765f
pietroferraiuolo <pietro.ferraiuolo@inaf.it>
2025-04-19 18:58:47 +0200

    Fix configuration folder variable assignment in iff_processing.py

--- aoptics/dmutils/iff_processing.py
+++ aoptics/dmutils/iff_processing.py
@@ -65,7 +65,7 @@ _config = _cp.ConfigParser()
 _imgFold = _fn.OPD_IMAGES_ROOT_FOLDER
 _ifFold = _fn.IFFUNCTIONS_ROOT_FOLDER
 _intMatFold = _fn.INTMAT_ROOT_FOLDER
-_confFold = _fn.CONFIGURATION_ROOT_FOLDER
+_confFold = _fn.CONFIGURATION_FOLDER
 _frameCenter = [200, 200]
 _ts = _osu.newtn
 
