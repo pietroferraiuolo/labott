345f69ec1cc92f15cd4d4e235f23c86d082f9253
Pietro Ferraiuolo <pietro.ferraiuolo@inaf.it>
2025-04-23 19:52:09 +0200

    Add main functionality and configuration handling for calpy command-line tool

---
+++ aoptics/__main__.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import shutil
+
+def check_dir(config_path):
+    if not os.path.exists(config_path):
+        os.makedirs(config_path)
+        if not os.path.isdir(config_path):
+            raise OSError(f"Invalid Path: {config_path}")
+    config_path = os.path.join(config_path, 'configuration.yaml')
+    return config_path
+
+def main():
+    # Check if ipython3 is installed
+    if not shutil.which("ipython3"):
+        print("Error: ipython3 is not installed or not in your PATH.")
+        sys.exit(1)
+    # if -h/--help is passed, show help message
+    if len(sys.argv) > 1 and sys.argv[1] in ['-h', '--help']:
+        print("""
+CALPY DOCUMENTATION
+`calpy` is a command-line tool that calls an interactive Python shell (ipython3)
+with the option to pass the path to a configuration file for the `aoptics` package.
+
+Options:
+    no option  : Initialize an ipython3 --pylab='qt' shell
+
+    -f <path>  : Pass the path to a configuration file for the automatically
+                imported `aoptics` package (e.g., '../aopticsConf/configuration.yaml')
+
+    -h |--help : Shows this help message
+
+        """)
+    elif len(sys.argv) > 2 and sys.argv[1] == '-f':
+        config_path = sys.argv[2]
+        if not '.yaml' in config_path: # os.path.isfile(config_path):
+            try:
+                config_path = check_dir(config_path)
+            except OSError as ose:
+                print(f"Error: {ose}")
+                sys.exit(1)
+        if '--create' in sys.argv:
+            from aoptics.core.root import create_configuration_file
+            create_configuration_file(config_path, data_path=True)
+        if not os.path.exists(config_path):
+            print(f"Error: The file {sys.argv[2]} does not exist.")
+            sys.exit(1)
+        os.system(f"export AOCONF={config_path} && ipython3 --pylab='qt' -i -c 'import aoptics'")
+    else:
+        os.system("ipython3 --pylab='qt'")
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 about = {}
 here = os.path.abspath(os.path.dirname(__file__))
@@ -9,6 +10,80 @@ with open(os.path.join(here, 'aoptics', '__version__.py'), 'r') as _:
 # Read dependencies from requirements.txt
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
+
+# class CalpyFuncInstall(install):
+#     """
+#     Custom install command to write the calpy function in the `.bashrc`
+#     or `.bash_functions` file.
+#     """
+#     def run(self):
+#         install.run(self)
+#         bashrc = os.path.expanduser("~/.bashrc")
+#         bashf  = os.path.expanduser("~/.bash_functions")
+#         source_check = """if [ -f ~/.bash_functions ]; then
+#     . ~/.bash_functions
+# fi"""
+#         calpy = """
+# # calpy function
+
+# calpy() {
+#     if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+#         cat << 'EOF'
+# CALPY DOCUMENTATION
+# `calpy` is a bash function that calls an interactive python shell (ipython3) with
+# the option to pass the path to a configuration file for the `aoptics` package.
+
+# Options
+# -------
+#         no option : Initialize an ipython3 --pylab='qt' shell
+
+# '-f'    pass the path to a configuration file for the automatically imported
+#         `aoptics` package (e.g. '../aopticsConf/configuration.yaml')
+
+# '-h'    Shows this help message
+
+# EOF
+
+#     elif [ "$1" == "-f" ]; then
+#         if [ -n "$2" ]; then
+#             export AOCONF="$2"
+#             ipython3 --pylab='qt' -i -c "import aoptics"
+#         else
+#             echo "Error: No configuration file provided. Use '-h' for help."
+#         fi
+#     else
+#         ipython3 --pylab='qt'
+#     fi
+# }
+
+# """
+#         # Check if the function is already in the file
+#         if not os.path.exists(bashrc):
+#             with open(bashrc, 'w') as f:
+#                 f.write("#!/bin/bash\n")
+#         with open(bashrc, 'r') as f:
+#             content = f.read()
+#         if not source_check in content:
+#             with open(bashrc, 'a') as f:
+#                 f.write(f"\n{source_check}\n")
+#         if not os.path.exists(bashf):
+#             with open(bashf, 'w') as f:
+#                 f.write("#!/bin/bash\n")
+#         with open(bashf, 'r') as f:
+#             content = f.read()
+#         try:
+#             if not calpy in content:
+#                 with open(bashf, 'a') as f: 
+#                     f.write(calpy)
+#                 print("\nThe `calpy` function has been written to ~/.bash_functions.\n\
+# Please source it with `source ~/.bashrc`.\nInstructions with `calpy -h`.")
+#             else:
+#                 print("\nThe `calpy` function is already in ~/.bash_functions.")
+#         except Exception as e:
+#             print(f"\nError writing the `calpy` function. {e}\n\
+# Please add the calpy function manually.")
+
+
 
 setup(
     name=about["__title__"],
@@ -25,10 +100,15 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Unix",
     ],
     python_requires=">=3.10",
     install_requires=requirements,
     include_package_data=True,
     package_data={'aoptics': ['core/_configurations/*.yaml']},
+    entry_points={
+        'console_scripts': [
+            'calpy=aoptics.__main__:main',
+        ],
+    },
 )
