3539c3d44e28af0379b89046fec305e2e9dee9ad
pietroferraiuolo <pietro.ferraiuolo>
2025-07-16 10:31:23 +0200

    Fix normalization of mirror modes matrix in _initMirrorModes method

--- opticalib/devices/_API/micAPI.py
+++ opticalib/devices/_API/micAPI.py
@@ -121,9 +121,9 @@ class BaseAdOpticaDm():
             Mirror Modes Matrix.
         """
         cmdMat = np.zeros((self.nActs, 222))
-        mirrorModes = np.array(self._aoClient.aoSystem.sysConf.gen.FFWDSvdMatrix[0])
-        cmdMat[:111,:111] = mirrorModes[0,:,:] / np.std(mirrorModes[0,0,:])
-        cmdMat[111:222,111:222] = mirrorModes[1,:,:] / np.std(mirrorModes[1,0,:])
+        mirrorModes = np.array(self._aoClient.aoSystem.sysConf.gen.FFWDSvdMatrix[0]) # (2,111,111)
+        cmdMat[:111,:111] = mirrorModes[0,:,:] / np.std(mirrorModes[0,:111,0])
+        cmdMat[111:222,111:222] = mirrorModes[1,:,:] / np.std(mirrorModes[1,111:222,0])
         return cmdMat
 
 
